a4cc83a4a92c02118af65e9047ff327d4b72b343
dlhocker <dlhocker@gmail.com>
2024-03-21 11:14:51 -0400

    adding init

---
+++ dynamics/process/__init__.py
@@ -0,0 +1,9 @@
+#init file for rnn process
+
+#from . import AR1
+#from . import cca
+#from . import divnorm
+#from . import kalman
+#from . import plds
+from . import rnn
+#from . import modelsystems
